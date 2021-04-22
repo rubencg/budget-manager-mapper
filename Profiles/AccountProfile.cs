738b43861d23de9c185d1cb44af7bad76a7c4b47
Ruben Cardenas <rubencg88@gmail.com>
2021-04-22 17:56:10 -0500

    Added color and image to accounts

--- Profiles/AccountProfile.cs
+++ Profiles/AccountProfile.cs
@@ -8,9 +8,56 @@ namespace budget_manager_mapper.Profiles
         public AccountProfile()
         {
             CreateMap<AccountIn, AccountOut>()
-                .ForMember(dest => dest.Color, opt => opt.MapFrom(o => "#4287f5"))
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(o => "money-bill"))
+                .ForMember(dest => dest.Color, opt => opt.MapFrom(o => GetColorForAccountName(o.Name)))
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(o => GetIconForAccountName(o.Name)))
                 .ForMember(dest => dest.AccountType, opt => opt.MapFrom(src => GetByType(src.Type)));
+        }
+
+        public static string GetIconForAccountName(string accountName){
+            switch (accountName)
+            {
+                case "Debito Ruben":
+                case "Sarahi Banregio":
+                case "Debito Sarahi":
+                    return "money-check";
+                case "Imprevistos Efectivo":
+                case "Efectivo Sarahi":
+                case "Efectivo Ruben":
+                    return "wallet";
+                case "Ahorro Inversion Sarahi":
+                case "Ahorro Casa Sarahi":
+                case "Tikitchen":
+                    return "piggy-bank";
+                case "Sarahi Credito":
+                case "Ruben Banamex Credito":
+                    return "money-check-alt";
+                default:
+                    return "hand-holding-usd";
+            }
+        }
+
+        public static string GetColorForAccountName(string accountName){
+            switch (accountName)
+            {
+                case "Debito Ruben":
+                    return "#000080";
+                case "Sarahi Banregio":
+                    return "#8d0d0d";
+                case "Debito Sarahi":
+                    return "#51cec5";
+                case "Imprevistos Efectivo":
+                    return "#4287f5";
+                case "Efectivo Sarahi":
+                    return "#23a5a5";
+                case "Efectivo Ruben":
+                    return "#d6b9d6";
+                case "Sarahi Credito":
+                    return "#b85cb8";
+                case "Ruben Banamex Credito":
+                    return "#8d1818";
+                default:
+                    return "#000080";
+            }
         }
 
         private AccountType GetByType(string typeId){
--- Program.cs
+++ Program.cs
@@ -12,14 +12,14 @@ namespace budget_manager_mapper
         {
             IProcessor categoryProcessor = new Processor();
             // Estas dos no se deberian de necesitar mas
-            // categoryProcessor.processFile<CategoryIn, CategoryOut>("jsons/expense-categories.json", "expense-categories-to-import.json");
-            // categoryProcessor.processFile<CategoryIn, CategoryOut>("jsons/income-categories.json", "income-categories-to-import.json");
+            // categoryProcessor.processFile<CategoryIn, CategoryOut>("jsons/expense-categories.json", "jsons/export/expense-categories-to-import.json");
+            // categoryProcessor.processFile<CategoryIn, CategoryOut>("jsons/income-categories.json", "jsons/export/income-categories-to-import.json");
 
             // A partir de aqui es la migracion
-            categoryProcessor.processFile<AccountIn, AccountOut>("jsons/accounts.json", "accounts-to-import.json");
-            categoryProcessor.processFile<TransferIn, TransferOut>("jsons/transfers.json", "transfers-to-import.json");
-            categoryProcessor.processFile<ExpenseIn, ExpenseOut>("jsons/expenses.json", "jsons/export/expenses-to-import.json");
-            categoryProcessor.processFile<IncomeIn, IncomeOut>("jsons/incomes.json", "incomes-to-import.json");
+            categoryProcessor.processFile<AccountIn, AccountOut>("jsons/accounts.json", "jsons/export/accounts-to-import.json");
+            // categoryProcessor.processFile<TransferIn, TransferOut>("jsons/transfers.json", "jsons/export/transfers-to-import.json");
+            // categoryProcessor.processFile<ExpenseIn, ExpenseOut>("jsons/expenses.json", "jsons/export/expenses-to-import.json");
+            // categoryProcessor.processFile<IncomeIn, IncomeOut>("jsons/incomes.json", "jsons/export/incomes-to-import.json");
             
         }
     }
